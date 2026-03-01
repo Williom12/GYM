a9b417a3a3bf7bb4a6071507064e80b0a29161bc
william Daza <125626916+Williom12@users.noreply.github.com>
2026-02-28 19:23:03 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,15 @@
-const CACHE_NAME="gym-cache";
+const CACHE="gym-cache-v1";
 
 self.addEventListener("install",e=>{
 e.waitUntil(
-caches.open(CACHE_NAME).then(cache=>{
-return cache.addAll(["./","index.html"]);
+caches.open(CACHE).then(cache=>{
+return cache.addAll(["./","./index.html"]);
 })
 );
 });
 
 self.addEventListener("fetch",e=>{
 e.respondWith(
-caches.match(e.request).then(res=>{
-return res||fetch(e.request);
-})
+caches.match(e.request).then(r=>r||fetch(e.request))
 );
 });
